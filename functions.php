8f9701677f36ca0122a9ac98c21c03e796189c57
Khayyam <65266683+khayyamkhalil@users.noreply.github.com>
2020-12-24 16:40:47 -0800

    Added automation (NodeJS etc..)

--- functions.php
+++ functions.php
@@ -1,11 +1,25 @@
 <?php 
 // Create and define a function to point to the stylesheet
 function university_files() {
-    wp_enqueue_script("university-main-js", get_theme_file_uri('/js/scripts-bundled.js'), NULL, "1.0", true); // Dependencies, version number, do you want to load this file right before the closing body tag.
+    // wp_enqueue_script("main-university-js", get_theme_file_uri("/js/scripts-bundled.js") , NULL, "1.0", true); 
     wp_enqueue_style("custom-google-fonts", "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
     wp_enqueue_style("font-awesome", "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
-    wp_enqueue_style("university_main_style", get_stylesheet_uri());
+    // wp_enqueue_style("university_main_styles", get_stylesheet_uri());
+
+    // wp_enqueue_script("main-university-js", "http://localhost:3000/bundled.js" , NULL, "1.0", true); 
+
+    if(strstr($_SERVER["SERVER_NAME"], "localhost")) {
+        wp_enqueue_script("main-university-js", "http://localhost:3000/bundled.js" , NULL, "1.0", true); 
+
+    } else {
+        wp_enqueue_script("our-vendors-js", get_theme_file_uri("/bundled-assets/vendors.js") , NULL, "1.0", true); 
+        wp_enqueue_script("main-university-js", get_theme_file_uri("/bundled-assets/scripts.js") , NULL, "1.0", true);
+        wp_enqueue_style("our-main-styles", get_theme_file_uri("/bundled-assets/styles.css"));         
+    }
+
 }
+
+  
 
 // To call the above function, we need to add a WORDPRESS function
 add_action("wp_enqueue_scripts", "university_files");
