95989b8f1de29940010c45df1dc45f818b2d9a95
Khayyam <65266683+khayyamkhalil@users.noreply.github.com>
2020-12-29 18:04:39 -0800

    Added custom event post functionality

--- functions.php
+++ functions.php
@@ -1,22 +1,15 @@
 <?php 
 // Create and define a function to point to the stylesheet
 function university_files() {
-    // wp_enqueue_script("main-university-js", get_theme_file_uri("/js/scripts-bundled.js") , NULL, "1.0", true); 
     wp_enqueue_style("custom-google-fonts", "//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i");
     wp_enqueue_style("font-awesome", "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
-    // wp_enqueue_style("university_main_styles", get_stylesheet_uri());
-
-    // wp_enqueue_script("main-university-js", "http://localhost:3000/bundled.js" , NULL, "1.0", true); 
-
     if(strstr($_SERVER["SERVER_NAME"], "localhost")) {
         wp_enqueue_script("main-university-js", "http://localhost:3000/bundled.js" , NULL, "1.0", true); 
-
     } else {
         wp_enqueue_script("our-vendors-js", get_theme_file_uri("/bundled-assets/vendors.js") , NULL, "1.0", true); 
         wp_enqueue_script("main-university-js", get_theme_file_uri("/bundled-assets/scripts.js") , NULL, "1.0", true);
         wp_enqueue_style("our-main-styles", get_theme_file_uri("/bundled-assets/styles.css"));         
     }
-
 }
 
   
@@ -36,3 +29,5 @@ function university_features () {
 }
 // To call the above function, for title of all of the pages
 add_action("after_setup_theme", "university_features");
+
+
